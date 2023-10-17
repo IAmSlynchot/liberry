8c0db4cbc8600ae610f4ae1e136303466817d34e
Sam Stone <slynch@DeepThought.local>
2023-10-17 15:57:02 -0400

    adding image gallery with pexels placeholder images

--- components/gallery.js
+++ components/gallery.js
@@ -1,7 +1,7 @@
 export default function Gallery() {
     return (
         <div>
-            
+            <h2>My LiBerry</h2>
         </div>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
+import React, { useState } from 'react';
 import { Inter } from 'next/font/google';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import Navbar from '../components/navbar';
 import LeftNav from '../components/left-nav'
+import { getCuratedPhotos, getQueryPhotos } from '../lib/api';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home({ children }) {
+export default function Home({ children, data }) {
+  const [photos, setPhotos] = useState(data);
   return (
       <>
       <Head>
@@ -16,8 +19,30 @@ export default function Home({ children }) {
         <main>
             <LeftNav />
             <h1 className="pageTitle">Welcome to LiBerry!!</h1>
+            <div>
+            {
+                photos.map((pic) => (
+                    <div key={pic.id}>
+                        <Image 
+                        src={pic.src.medium} 
+                        width="200" 
+                        height="200"
+                        alt={pic.url}  />
+                    </div>
+                ))
+            }
+            </div>
             <Link href="about">About</Link>
         </main>
       </>
   )
 }
+
+export async function getServerSideProps() {
+    const data = await getCuratedPhotos();
+    return {
+      props: {
+        data,
+      },
+    };
+  }
