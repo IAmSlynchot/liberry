6dc632c49c3a6f43171dbf321622a4692fd00a1a
Sam Stone <slynchot@gmail.com>
2023-10-26 19:36:51 -0400

    adding image storage to db

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "imageliberry": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
